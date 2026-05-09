6dc10252ba1184ca0a784a0ce0f991d433070e0d
BaoPhan-st <23130023@st.hcmuaf.edu.vn>
2026-05-09 22:20:10 +0700

    Tao board

---
+++ Data/PropertyData.gd
@@ -0,0 +1,15 @@
+extends CellData
+class_name PropertyData
+
+@export var color_name: String
+@export var color_code: Color
+
+@export var buy_price: int
+
+@export var house_1_rent: int
+@export var house_2_rent: int
+@export var house_3_rent: int
+@export var house_4_rent: int
+@export var hotel_rent: int
+
+@export var build_cost: int
